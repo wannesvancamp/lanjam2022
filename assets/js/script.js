6487bf49e5ca3dd69562c4bcec4ad90568522a21
Wannes Van Camp <wannes.van.camp@student.howest.be>
2022-03-12 20:06:50 +0100

    working on calculating the rarities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
-<<<<<<< HEAD
-console.log("help")
-=======
-console.log('Help')
->>>>>>> bc34a3f8caaf66377bb52b2a5fabfd5c4a7e14ff
+document.addEventListener("DOMContentLoaded", init)
+
+function init(){
+    calculate()
+}
+
+function calculate(){
+    let maxValues = [41, 15, 7, 20]
+    let actualValues = [3, 5, 2, 5]
+    let max;
+    let actual;
+    max = maxValues.reduce((a, b) => a + b, 0)
+    actual = actualValues.reduce((a,b) => a + b, 0)
+    console.log("Maximum: " + max);
+    console.log("Actual: " + actual)
+
+    console.log("The rarity is: " + (actual/max) * 100)
+}
+
+function calculateRarityJam(){
+    let maximum = 41
+    let now = 3
+}
