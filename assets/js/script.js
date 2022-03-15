95469107fdc6581399fdbdd9784cc9fd2ebe0c4c
Wannes Van Camp <wannes.van.camp@student.howest.be>
2022-03-15 17:13:27 +0100

    added moving background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", init)
 
 let NUM = 0
-
+let backgroundNum = 0
 function init() {
     calculate()
     gradient_c();
+    backgroundMove()
 }
 
 function calculate() {
@@ -32,14 +33,24 @@ function gradient_c(){
     var refresh=10; // Refresh rate in milli seconds
     mytime=setTimeout('gradient_ct()',refresh)
     }
-    function gradient_ct() {
-        if(NUM < 360){
-            NUM++;
-        } else {
-            NUM = 0
-        }
-        
-        document.querySelector("input[type='submit']").style.background = "linear-gradient(" + NUM + "deg, #eec0c6 0%, #7ee8fa 100%)"   
-        gradient_c();
-     }
+function gradient_ct() {
+    if(NUM < 360){
+        NUM++;
+    } else {
+        NUM = 0
+    }
     
+    document.querySelector("input[type='submit']").style.background = "linear-gradient(" + NUM + "deg, #eec0c6 0%, #7ee8fa 100%)"   
+    gradient_c();
+    }
+    
+function backgroundMove(){
+    let refresh = 50
+    time = setTimeout('backgroundMover()', refresh)
+}
+function backgroundMover(){
+    backgroundNum += 0.02
+    console.log(backgroundNum)
+    document.querySelector("body").style.backgroundPosition = backgroundNum + "rem 0"
+    backgroundMove();
+}
