5a06b61efd81737e1c22f8c8bd35fe6c3191ce95
Wim Van Renterghem <git@vrwim.com>
2022-04-01 17:25:00 +0200

    Fix collection

--- assets/js/collection.js
+++ assets/js/collection.js
@@ -7,33 +7,12 @@ async function init() {
     await loadImages();
 }
 
-function extraNumbers(num){
-    let format = ".json";
-    let paddedNum = num.toString().padStart(64, 0);
-    let metadataJSON = paddedNum.concat(format);
-    return metadataJSON;
-}
-
-function toHex(number){
-  if (number < 0)
-  {
-    number = 0xFFFFFFFF + number + 1;
-  }
-
-  return number.toString(16);
+function padHex(num){
+    return num.toString(16).padStart(64, 0);
 }
 
 async function loadImages(){
     for(let i = 1;i<101;i++){
-            await delay(20);
-            fetch('https://gateway.pinata.cloud/ipfs/QmdMgy9N1qCMmbYrQJQyYvyuC1TqiuATMbPgBS3wRmMVuX/' + extraNumbers(toHex(i)))
-            .then((res) => res.json())
-            .then((data) => {  
-                let imgLink = "https://ipfs.io/ipfs/" + data.image.substr(7);
-                document.querySelector("main").innerHTML += `<img src="${imgLink}" alt="nft">`;
-            }).catch(function(err){
-                console.error(err)
-            })
-
+        document.querySelector("main").innerHTML += `<img src="https://ipfs.io/ipfs/QmZXrFU6w4uY2hwu2T7hcqZDaPQ6QqmT6uUhKCKJcaF4aG/${padHex(i)}.png" alt="nft" loading="lazy">`;
     }
 }
